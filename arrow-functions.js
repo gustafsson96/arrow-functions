7296b744e53027fabc13c8a4ee6e5ddac15cc3c2
gustafsson96 <>
2023-07-25 14:22:14 +0000

    Add different functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,32 @@
 
 // Vanilla JavaScript Function
 
+// function addTwoNumbers(a, b) {
+//    // Code block
+//    return a + b;
+//}
+//let sum = addTwoNumbers(3, 5);
+//console.log(sum);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => a + b;
+
 // Implicit Returns
+// const saySomething = message => console.log(message);
+// saySomething('Hello there!');
+
+const sayHello = () = => console.log('hello');
+sayHello();
 
 // Returning Multiple Lines
